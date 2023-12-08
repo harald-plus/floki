8c7fb5c0b5f6ff6428a95ab09ff84baf377705ee
Harald+ <h@hrld.no>
2023-12-09 00:02:19 +0100

    IDK

--- index.js
+++ index.js
@@ -3,45 +3,28 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-// Twitter API credentials
-const apiKey = process.env.X_API_KEY; // Use the names you set in Vercel
+// Twitter API credentials from environment variables
+const apiKey = process.env.X_API_KEY;
 const apiSecretKey = process.env.X_API_SECRET_KEY;
 const accessToken = process.env.X_ACCESS_TOKEN;
 const accessTokenSecret = process.env.X_ACCESS_TOKEN_SECRET;
 const bearerToken = process.env.X_BEARER_TOKEN;
 
-// Initialize express
+// Initialize express and cors
 const app = express();
-const port = 3000;
-
-// Middleware to set CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://floki.com'); // Allow your frontend domain
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allowed headers
-
-    // Handle preflight requests for CORS
-    if ('OPTIONS' === req.method) {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
+app.use(cors({ origin: 'https://floki.com' })); // Simplified CORS setup
+const port = process.env.PORT || 3000; // Use environment variable for port, with a fallback
 
 // Endpoint to get follower count
 app.get('/getFollowerCount', async (req, res) => {
     try {
-        // Make request to Twitter API
         const response = await axios.get('https://api.twitter.com/1.1/users/show.json?screen_name=RealFlokiInu', {
             headers: {
                 'Authorization': `Bearer ${bearerToken}`
             }
         });
 
-        // Extract follower count
         const followerCount = response.data.followers_count;
-
-        // Send response
         res.json({ followerCount });
     } catch (error) {
         console.error(error);
